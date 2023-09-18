7d4725ad43bf75f0df1ad9af6075977ca9823513
Michael <mdefrance1988@gmail.com>
2023-09-19 00:04:56 +0200

    feat: Update UI and use KivyMD components
    This commit updates the user interface of the NumerologyNameAnalyzer app
    by incorporating KivyMD components for a more modern and polished look.
    It also includes a custom color palette and theme style, improving the overall
    user experience.
    
    Changes Made:
    - Updated import statements to include necessary Kivy and KivyMD modules.
    - Added a custom color palette and theme style to the app.
    - Created a custom toolbar with a title label.
    - Used MDTextField for the name input field with customized line color.
    - Used MDRaisedButton for the conversion button with a customized background color.
    - Used MDLabel for displaying results.
    - Organized the UI elements in a more structured way using BoxLayout.

--- main.py
+++ main.py
@@ -1,139 +1,138 @@
+# IMPORTS
 import os
-from kivy.uix.video import Video
-from kivy.core.window import Window
-from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.image import Image
 from kivy.uix.label import Label
-from kivymd.app import MDApp
-from kivymd.uix.button import MDRaisedButton
-from kivymd.uix.textfield import MDTextField
-from kivymd.uix.label import MDLabel
-from kivymd.uix.screen import MDScreen
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.video import Video
+from kivy.uix.boxlayout import BoxLayout  # Import BoxLayout
+from kivy.animation import Animation
+from kivy.core.video import Video
+from kivy.uix.videoplayer import VideoPlayer
 
-# Add this import at the beginning of your script
-from kivy.uix.popup import Popup
+# Install ffpyplayer to play videos.
+
+# VARIABLES
+os.environ["KIVY_VIDEO"] = "ffpyplayer"
+LOGO = '3.png'
 
 
-class NumerologyNameAnalyzerApp(MDApp):
+class NameDecoder(App):
     """
-    The main application class for NumerologyNameAnalyzer.
-
-    This class defines the structure and behavior of the NumerologyNameAnalyzer app.
-    It includes the user interface setup, name-to-number conversion logic, and video playback.
-
-    Attributes:
-        None
-
-    Methods:
-        - build(self): Build the app's user interface.
-        - convert_name(self, instance): Handle name conversion and video playback.
-        - calculate_number(self, name): Calculate the numerology number for a given name.
-        - play_video(self, number): Play the associated video explanation.
+    A Kivy application for numerology name analysis.
     """
 
     def build(self):
         """
-        Build the user interface of the NumerologyNameAnalyzer app.
-
-        This method constructs the app's user interface, including a logo, input box,
-        and 'Convert' button.
-
-        Parameters:
-            self: The NumerologyNameAnalyzerApp instance.
+        Build the main application window.
 
         Returns:
-            layout (BoxLayout): The root layout of the app's user interface.
+            GridLayout: The root layout of the application.
         """
-        # Customize the color palette
-        self.theme_cls.primary_palette = "Purple"  # Set the primary color
-        self.theme_cls.primary_hue = "400"  # Choose the hue (e.g., "400")
-        self.theme_cls.theme_style = "Dark"  # Choose the dark theme
+        self.window = GridLayout()
+        self.window.cols = 1
 
-        screen = MDScreen()
+        # Image size margin (sides, top and bottom)
+        self.window.size_hint = (0.60, 0.70)
+        # Image position (horizontally, vertically)
+        self.window.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
 
-        # Create a BoxLayout for content with a vertical orientation
-        content = BoxLayout(orientation="vertical")
+        return self.window
 
-        # Create a custom toolbar using a BoxLayout
-        toolbar = BoxLayout(
-            orientation="horizontal",
-            size_hint=(1, None),
-            height=56,  # Adjust the height as needed
-            spacing=10,
-        )
-        title_label = Label(
-            text="Numerology Name Analyzer",
-            size_hint_x=None,
-            width=Window.width,  # Adjust the width as needed
-            bold=True,
-            color=(1, 1, 1, 1),  # Set the text color to white (R,G,B,A)
-        )
-
-        toolbar.add_widget(title_label)
-
-        # Create a BoxLayout for the middle widget (name input and video) with a vertical orientation
-        middle_widget = BoxLayout(orientation="vertical", size_hint=(1, None), height=300)
-
-        # Create an MDTextField for name input with a customized line color
-        name_input = MDTextField(
-            hint_text="Enter your name",
-        )
-        name_input.line_color = self.theme_cls.primary_color  # Set the line color
-
-        # Create an MDRaisedButton for conversion with a customized color
-        convert_button = MDRaisedButton(
-            text="Convert",
-            on_release=self.convert_name,
-            md_bg_color=self.theme_cls.primary_color,  # Use the primary color
-        )
-
-        middle_widget.add_widget(name_input)  # Add the name input field here
-        middle_widget.add_widget(convert_button)
-
-        # Create an MDLabel for displaying results
-        self.result_label = MDLabel()
-        middle_widget.add_widget(self.result_label)
-
-        # Add the middle widget to the content layout
-        content.add_widget(toolbar)
-        content.add_widget(middle_widget)
-
-        screen.add_widget(content)
-        return screen
-
-    def convert_name(self, instance):
+    def on_start(self):
         """
-        Handle name conversion and video playback.
+        Initialize and animate the application elements when it starts.
 
-        This method is triggered when the user clicks the 'Convert' button. It retrieves
-        the user-entered name, calculates the numerology number, and plays the associated
-        video explanation.
-
-        Parameters:
-            self: The NumerologyNameAnalyzerApp instance.
-            instance: The button instance that triggered the event.
-
-        Returns:
-            None
+        This function sets up the logo animation and initializes input box and
+        convert button, making them visible with a fade-in animation.
         """
+        # Logo widget with initial opacity of 0
+        self.logo = Image(source=LOGO, allow_stretch=False, opacity=0)
+        self.window.add_widget(self.logo)
 
-        user_name = self.root.children[1].text
-        converted_number = self.calculate_number(user_name)
-        self.result_label.text = f"Numerology Number: {converted_number}"
-        self.play_video(converted_number)
+        # Sequential animation: Fade in the logo, then input box and button
+        fade_in_logo = Animation(opacity=1, duration=3)
+        fade_in_input_button = Animation(opacity=1, duration=3)
+
+        # Define a callback function for the end of the fade-in logo animation
+        def on_logo_fade_in_finish(animation, widget):
+            # After the logo fades in, start the fade-in animation for input box and button
+            fade_in_input_button.start(self.input_box)
+            fade_in_input_button.start(self.convert_button)
+
+        # Bind the callback function to the end of the fade-in logo animation
+        fade_in_logo.bind(on_complete=on_logo_fade_in_finish)
+
+        # Input box and convert button with initial opacity of 0
+        self.input_box = TextInput(multiline=False,
+                                   hint_text='Enter your name',
+                                   padding=(10, 20),
+                                   size_hint=(1, 0.20),
+                                   opacity=0
+                                   )
+
+        self.convert_button = Button(text='Convert',
+                                     size_hint=(1, 0.20),
+                                     bold=True,
+                                     background_color=("#FC3030"),
+                                     background_normal='',
+                                     opacity=0
+                                     )
+
+        # Bind the on_release event of the button to the convert_name method
+        self.convert_button.bind(on_release=self.convert_name)
+
+        # Add the input box and convert button to the window (initially invisible)
+        self.window.add_widget(self.input_box)
+        self.window.add_widget(self.convert_button)
+
+        # Start the fade-in logo animation
+        fade_in_logo.start(self.logo)
+
+    def convert_name(self, _):
+        """
+        Convert the entered name to a numerology number and play the associated video.
+
+        Args:
+            _: Unused argument.
+        """
+        # Remove all widgets from the window
+        self.window.clear_widgets()
+
+        # Create a layout to hold the video player
+        video_layout = BoxLayout(orientation='vertical')
+
+        # Create and add the video player to the layout
+        video_filename = f"{self.calculate_number(self.input_box.text)}.mp4"
+        video_path = os.path.join(os.path.dirname(__file__), video_filename)
+        if os.path.exists(video_path):
+            video_player = VideoPlayer(source=video_path, state='play')
+            video_layout.add_widget(video_player)  # Add the VideoPlayer to the layout
+            self.window.add_widget(video_layout)  # Add the layout to the window
+        else:
+            # Use a placeholder video with a message
+            not_found_video_path = os.path.join(os.path.dirname(__file__), "not_found.mp4")
+            if os.path.exists(not_found_video_path):
+                video_player = VideoPlayer(source=not_found_video_path, state='play')
+                video_layout.add_widget(video_player)
+            else:
+                # Display a message if the placeholder video is also not found
+                label = Label(text=f"Video '{video_filename}' not found.")
+                video_layout.add_widget(label)
+                self.window.add_widget(video_layout)  # Add the layout to the window
+                print(f"Video '{video_filename}' not found.")
 
     def calculate_number(self, name):
         """
         Calculate the numerology number for a given name.
 
-        This method takes a user-entered name and calculates the corresponding numerology
-        number based on Pythagorean numerology and the provided vibrational correlations.
-
-        Parameters:
-            self: The NumerologyNameAnalyzerApp instance.
-            name (str): The user-entered name.
+        Args:
+            name (str): The name to be converted.
 
         Returns:
-            str: The numerology number as a string.
+            str: The numerology number calculated from the name.
         """
         name = name.upper()  # Convert the name to uppercase for consistency
         number_mapping = {
@@ -164,31 +163,16 @@ class NumerologyNameAnalyzerApp(MDApp):
         return str(total)
 
     def play_video(self, number):
-        """
-        Play the associated video explanation for a numerology number.
-
-        This method plays the video explanation associated with the numerology number
-        provided as an argument. It retrieves and displays the video from the 'partend'
-        directory.
-
-        Parameters:
-            self: The NumerologyNameAnalyzerApp instance.
-            number (str): The numerology number as a string.
-
-        Returns:
-            None
-        """
         video_filename = f"{number}.mp4"
         video_path = os.path.join(os.path.dirname(__file__), video_filename)
 
         if os.path.exists(video_path):
             video = Video(source=video_path, state='play')
-            video.popup = Popup(title='Numerology Video', content=video, size_hint=(None, None),
-                                size=(Window.width * 0.8, Window.height * 0.8))
-            video.popup.open()
+            self.window.clear_widgets()  # Clear existing widgets
+            self.window.add_widget(video)  # Add the video to the layout
         else:
             print(f"Video '{video_filename}' not found.")
 
 
 if __name__ == '__main__':
-    NumerologyNameAnalyzerApp().run()
+    NameDecoder().run()
